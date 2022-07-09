492a2e908d51e50dbd3f9cca89eae1d129ed8cdc
Midelpic <108611812+Midelpic@users.noreply.github.com>
2022-07-09 14:36:46 +0000

    daily commit

---
+++ script.js
@@ -0,0 +1,14 @@
+require("@minomi/minomi.db")
+
+require("@minomi/minomi.db");
+minomi.write('Cool', 'Package'); // Writes "Cool" as key and "Package" as value in the database.
+console.log(minomi.get("")) // returns Package
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
